c32f617b45c17064d077d722ed54b2f0b8c78c0e
fromgate <fromgate@gmail.com>
2014-08-12 13:39:40 +0400

    v0.9.3

--- src/me/fromgate/reactions/util/ParamUtil.java
+++ src/me/fromgate/reactions/util/ParamUtil.java
@@ -26,12 +26,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import me.fromgate.reactions.RAUtil;
-import me.fromgate.reactions.ReActions;
 
 public class ParamUtil {
-    
-    
     /////////////////////////////////////////////////////////////////////////////////////////////////////
     // преобразуем строку вида <команда> <параметр=значение> <параметр=значение> <параметр={значение в несколько слов}> в Map {команда="", параметр=значеие....}
 	public static Map<String,String> parseParams(String param){
@@ -61,10 +57,6 @@ public class ParamUtil {
     	return params;
     }
   
-    private static RAUtil u(){
-        return ReActions.util;
-    }
-    
     public static String getParam(Map<String,String> params, String key, String defparam){
         if (!params.containsKey(key)) return defparam;
         return params.get(key);
@@ -73,15 +65,14 @@ public class ParamUtil {
     public static int getParam(Map<String,String> params, String key, int defparam){
         if (!params.containsKey(key)) return defparam;
         String str = params.get(key);
-        //if (!str.matches("[1-9]+[0-9]*")) return defparam;
-        if (!u().isIntegerGZ(str)) return defparam;
+        if (!str.matches("-?[1-9]+[0-9]*")) return defparam;
         return Integer.parseInt(str);
     }
 
     public static float getParam(Map<String,String> params, String key, float defparam){
         if (!params.containsKey(key)) return defparam;
         String str = params.get(key);
-        if (!str.matches("[0-9]+\\.?[0-9]*")) return defparam;
+        if (!str.matches("-?[0-9]+\\.?[0-9]*")) return defparam;
         return Float.parseFloat(str);
     }
 
