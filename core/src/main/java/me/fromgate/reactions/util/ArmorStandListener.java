5ab79c4182a4cc790c6c1859129d1293c7d729ec
fromgate <fromgate@gmail.com>
2017-03-22 11:46:54 +0300

    Update version (plugin.yml)

--- core/src/main/java/me/fromgate/reactions/util/ArmorStandListener.java
+++ core/src/main/java/me/fromgate/reactions/util/ArmorStandListener.java
@@ -2,12 +2,15 @@ package me.fromgate.reactions.util;
 
 import me.fromgate.reactions.event.EventManager;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 
 public class ArmorStandListener implements Listener {
 
 
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = false)
     public void onPlayerInteractAtEntityEvent(PlayerInteractAtEntityEvent event) {
         if (event.getRightClicked() instanceof LivingEntity)
             EventManager.raiseMobClickEvent(event.getPlayer(), (LivingEntity) event.getRightClicked());
