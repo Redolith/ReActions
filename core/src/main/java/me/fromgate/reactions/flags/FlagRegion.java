e8580bf7c825c0fc0b53105cb1700973048aa57f
fromgate <fromgate@gmail.com>
2017-05-12 23:10:08 +0300

    Lesser magic numbers...

--- core/src/main/java/me/fromgate/reactions/flags/FlagRegion.java
+++ core/src/main/java/me/fromgate/reactions/flags/FlagRegion.java
@@ -28,6 +28,12 @@ import org.bukkit.entity.Player;
 
 public class FlagRegion extends Flag {
 
+    final static int REGION = 0;
+    final static int REGION_PLAYERS = 1;
+    final static int REGION_MEMBER = 2;
+    final static int REGION_OWNER = 3;
+    final static int REGION_STATE = 4;
+
     private int flagType = 0;
 
     public FlagRegion(int flagType) {
@@ -35,19 +41,19 @@ public class FlagRegion extends Flag {
     }
 
     @Override
-    public boolean checkFlag(Player p, String param) {
+    public boolean checkFlag(Player player, String param) {
         if (!RAWorldGuard.isConnected()) return false;
         switch (flagType) {
-            case 0:
-                return RAWorldGuard.isPlayerInRegion(p, param);
-            case 1:
+            case REGION:
+                return RAWorldGuard.isPlayerInRegion(player, param);
+            case REGION_PLAYERS:
                 return playersInRegion(param);
-            case 2:
-                return RAWorldGuard.isPlayerIsMember(p, param);
-            case 3:
-                return RAWorldGuard.isPlayerIsOwner(p, param);
-            case 4:
-                return RAWorldGuard.isFlagInRegion(p, param);
+            case REGION_MEMBER:
+                return RAWorldGuard.isPlayerIsMember(player, param);
+            case REGION_OWNER:
+                return RAWorldGuard.isPlayerIsOwner(player, param);
+            case REGION_STATE:
+                return RAWorldGuard.isFlagInRegion(player, param);
         }
         return false;
     }
