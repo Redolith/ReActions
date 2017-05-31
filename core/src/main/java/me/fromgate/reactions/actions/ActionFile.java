892a7b932b3f2970d90eb2305c0695e29eb5d5a8
MaxDikiy <maxdik@maxdik.ru>
2017-05-31 12:33:53 +0300

    Modified FILE Action. Added actions: copy and move. Added addition placeholder group

--- core/src/main/java/me/fromgate/reactions/actions/ActionFile.java
+++ core/src/main/java/me/fromgate/reactions/actions/ActionFile.java
@@ -5,6 +5,9 @@ import me.fromgate.reactions.util.Variables;
 import org.bukkit.entity.Player;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 /**
  * Created by MaxDikiy on 5/7/2017.
@@ -14,6 +17,7 @@ public class ActionFile extends Action {
     public boolean execute(Player p, Param params) {
         String action = params.getParam("action", "");
         String fileName = params.getParam("fileName", "");
+        String fileNameTo = params.getParam("fileNameTo", "");
         if (action.isEmpty() || fileName.isEmpty()) return false;
 
 
@@ -36,6 +40,26 @@ public class ActionFile extends Action {
             }
             Variables.setTempVar("removecount", Integer.toString(c));
             return true;
+
+        } else {
+            if (fileNameTo.isEmpty()) return false;
+            File fileTo = new File(dir + "/" + fileNameTo);
+            try {
+                File fileToDir = new File(fileTo.getCanonicalPath());
+                if (!fileToDir.exists()) fileToDir.mkdirs();
+                if (file.isFile()) {
+                    if (action.equalsIgnoreCase("copy")) {
+                        Files.copy(file.toPath(), fileTo.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                    }
+                    if (action.equalsIgnoreCase("move")) {
+                        Files.move(file.toPath(), fileTo.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                    }
+                    return true;
+                }
+            } catch (IOException e) {
+                Variables.setTempVar("filedebug", e.getLocalizedMessage());
+            }
+
         }
         return false;
     }
